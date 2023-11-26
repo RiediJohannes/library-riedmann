ffc5ee540fa4a6db150ce753ce33002718fc613d
Johannes Riedmann <ae9614@student.jamk.fi>
2023-11-26 19:41:06 +0200

    Set up theming

--- final/src/lib/main.js
+++ final/src/lib/main.js
@@ -1,5 +1,11 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.documentElement.classList.add('is-dark');
+$(document).ready(function () {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function () {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
 });
