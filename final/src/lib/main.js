5e8b2f8bb42f4d503aff09a7856bec062c468b7b
Johannes Riedmann <ae9614@student.jamk.fi>
2023-12-02 22:51:14 +0200

    Synchronised related animations

--- final/src/lib/main.js
+++ final/src/lib/main.js
@@ -1,7 +1,12 @@
 "use strict";
 
-$(document).ready(function () {
+// synchronise animations that need to start playing at the same time in order to look good
+import sync from 'css-animation-sync';
+sync(['wield-sword', 'wield-shield']);
+sync(['move-pen', 'write-scribble-1', 'write-scribble-2']);
 
+// functionality of the hamburger menu on mobile
+$(document).ready(function () {
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function () {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
