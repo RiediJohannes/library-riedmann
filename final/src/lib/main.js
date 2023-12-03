21ee6f95ca2976d69c7f1d94efe9e6ac2c289087
Johannes Riedmann <ae9614@student.jamk.fi>
2023-12-03 13:59:57 +0200

    Removed animations sync - not needed

--- final/src/lib/main.js
+++ final/src/lib/main.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// synchronise animations that need to start playing at the same time in order to look good
-import sync from 'css-animation-sync';
-sync(['wield-sword', 'wield-shield']);
-sync(['move-pen', 'write-scribble-1', 'write-scribble-2']);
-
 // functionality of the hamburger menu on mobile
 $(document).ready(function () {
     // Check for click events on the navbar burger icon
