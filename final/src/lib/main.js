57e0df3d2643034a3618577aa43b8ccb28235b84
Johannes Riedmann <ae9614@student.jamk.fi>
2023-12-03 15:39:01 +0200

    Added favicons in various sizes

--- final/src/lib/main.js
+++ final/src/lib/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// functionality of the hamburger menu on mobile
 $(document).ready(function () {
+
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function () {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
