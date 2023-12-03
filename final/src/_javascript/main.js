ace9b2a01e4c91dc4f35398bdf448ebe05e28a7e
Johannes Riedmann <ae9614@student.jamk.fi>
2023-12-03 17:32:59 +0200

    Added simple functionality to shopping cart

--- final/src/_javascript/main.js
+++ final/src/_javascript/main.js
@@ -7,3 +7,35 @@ $(document).ready(function() {
       $(".navbar-menu").toggleClass("is-active");
   });
 });
+
+
+// clear the shopping cart
+$("#clear-button").click(() => {
+  $(".clear-target").each((i, obj) => {
+    $(obj).empty();
+  });
+
+  // reset the total price
+  $("#total-price").text("0,00€");
+
+  // unfocus the clear button
+  $("#clear-button").blur();
+
+  // disable every buy button
+  $(".buy-button").each(function () {
+    $(this).prop("disabled", true);
+  });
+});
+
+
+$(".buy-button").each(function () {
+  let button = $(this); // raw dom element
+  button.on("click", function() {
+    document.location.href = "index.html";
+  });
+});
+
+
+function addToCart() {
+  document.location.href = "../index.html";
+}
--- final/src/lib/main.js
+++ final/src/lib/main.js
@@ -2,10 +2,39 @@
 
 $(document).ready(function () {
 
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function () {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-    });
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
 });
+
+// clear the shopping cart
+$("#clear-button").click(function () {
+  $(".clear-target").each(function (i, obj) {
+    $(obj).empty();
+  });
+
+  // reset the total price
+  $("#total-price").text("0,00€");
+
+  // unfocus the clear button
+  $("#clear-button").blur();
+
+  // disable every buy button
+  $(".buy-button").each(function () {
+    $(this).prop("disabled", true);
+  });
+});
+
+$(".buy-button").each(function () {
+  var button = $(this); // raw dom element
+  button.on("click", function () {
+    document.location.href = "index.html";
+  });
+});
+
+function addToCart() {
+  document.location.href = "../index.html";
+}
